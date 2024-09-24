5951f3cfb4b34349b2efaebeaacf960f814dc716
your_username <mihirraj.mr5@gmail.com>
2024-09-24 10:52:38 +0530

    solved error for tag and about create post

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -17,8 +17,14 @@ const CreatePost = () => {
     const postTitle = postTitleElement.current.value;
     const postBody = postBodyElement.current.value;
     const reaction = reactionElement.current.value;
-    const tags = tagsElement.current.value.split(/(\s+)/);
+    const tags = tagsElement.current.value.split(" ");
 
+    userIdElement.current.value = "";
+    postTitleElement.current.value = "";
+    postBodyElement.current.value = "";
+    reactionElement.current.value = "";
+    tagsElement.current.value = "";
+    
     addPost(userId, postTitle, postBody, reaction, tags);
   };
 
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -10,10 +10,7 @@ const Post = ({ post }) => {
       <div className="card-body">
         <h5 className="card-title">
           {post.title}
-          <span className="position-absolute top-0 start-0 translate-middle badge rounded-pill bg-success">
-            <MdOutlineAddReaction>{post.reaction}</MdOutlineAddReaction>
-            <span className="visually-hidden">unread messages</span>
-          </span>
+
           <span
             className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
             onClick={() => deletePost(post.id)}
@@ -24,7 +21,9 @@ const Post = ({ post }) => {
         </h5>
         <p className="card-text">{post.body}</p>
         {post.tags.map((tag) => (
-          <span  key={tag} className="badge rounded-pill text-bg-primary hastag">{tag}</span>
+          <span key={tag} className="badge rounded-pill text-bg-primary hastag">
+            {tag}
+          </span>
         ))}
         <div className="alert alert-success reactions" role="alert">
           This post has been reacted by {post.reaction} people.
